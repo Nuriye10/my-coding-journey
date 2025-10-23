75441ab57cf08aea48ff5805a92397349ff46a96
Nuriye10 <nadirnurani@gmail.com>
2025-10-23 14:20:22 +0200

    practice javascript data type

--- variable.js
+++ variable.js
@@ -1,4 +1,9 @@
-var x =55;
-var X ="abc";
-var _x = 1.5;
-document.writeln(x);
+let x = 15;//this is a number
+let name = "Nuriye";//This is a string
+let u ; //undefined
+let numberString = "12345"+ "10";//This is a string that looks like a number
+
+document.writeln(typeof (x));
+document.writeln(name);
+document.writeln(u);
+document.writeln(typeof (numberString));
