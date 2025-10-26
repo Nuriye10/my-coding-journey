cd6217759ed617ffc42cb02f5fd2ef86b189a18a
Nuriye10 <nadirnurani@gmail.com>
2025-10-26 16:12:56 +0100

    learned js variable operators

--- variable.js
+++ variable.js
@@ -1,9 +1,12 @@
-let x = 15;//this is a number
-let name = "Nuriye";//This is a string
-let u ; //undefined
-let numberString = "12345"+ "10";//This is a string that looks like a number
+let x = 5;
+let y = 2;
+let z = x + y;
 
-document.writeln(typeof (x));
-document.writeln(name);
-document.writeln(u);
-document.writeln(typeof (numberString));
+let n = 5;
+let m = 2;
+let p = n * m;
+
+
+document.writeln(z); // Outputs: 7
+document .writeln(p);
+
