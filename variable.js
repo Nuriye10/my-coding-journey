cf7f021d7e545873de59e32e22e980c1aadb5967
Nuriye10 <nadirnurani@gmail.com>
2025-10-26 18:42:55 +0100

    js operators comparision

--- variable.js
+++ variable.js
@@ -6,7 +6,52 @@ let n = 5;
 let m = 2;
 let p = n * m;
 
+let h = 10;
+h *= 2; // Same as: h = h * 2
+
+
+// Subtraction assignment operator example
+let x2 = 10;
+x2 -= 5; // Same as: x2 = x2 - 5
 
 document.writeln(z); // Outputs: 7
 document .writeln(p);
+document.writeln(h); // Outputs: 20
+document.writeln(x2); // Outputs: 5
+
+
+ let one = 1;
+ let two = 2;
+ // comparision
+ //strict equality
+
+document.writeln(one===one); //true
+document.writeln(one===two);//false
+//
+//strict inequality
+
+document.writeln(one !==one);//false
+document.writeln(one !==two);//true
+
+//equality
+document.writeln(one == one);// true
+document.writeln(one== "1"); //true
+
+//inequality 
+document.writeln(one != one);//false
+document.writeln(one != "1");// false
+
+//Strict equality/inequality with strings
+document.writeln(one==="1"); //false
+document.writeln(one !== "1"); //true)
+
+
+
+
+     
+
+
+
+
+
 
