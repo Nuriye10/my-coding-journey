757567cc849b969639f5813068cf2bff49a420ca
Nuriye10 <nadirnurani@gmail.com>
2025-11-01 15:10:09 +0100

    did the task

--- variable.js
+++ variable.js
@@ -1,17 +1,5 @@
-let x = 10;
-let y = "Hi";
-let z = true;
-let a = "123";
-
-document.writeln(typeof(a));
-document.writeln(typeof(x));
-document.writeln(typeof(y));
-document.writeln(typeof(z));
-
-let myNumber = "74";
-myNumber += 3;
-document.writeln(myNumber);
-
+ let mySentence = "I'm sentencs number 3 ,and I was printed from JS file " 
+ document.writeln(mySentence);
 
 
 
