3ac09fde5062043f565f9461320913930d0152da
Nuriye10 <nadirnurani@gmail.com>
2025-11-02 20:02:54 +0100

    done with the task 2

--- variable.js
+++ variable.js
@@ -1,6 +1,31 @@
  let mySentence = "I'm sentencs number 3 ,and I was printed from JS file " 
  document.writeln(mySentence);
 
+ //task 2
+
+ let a = "a" + 1+2+1.2;
+ document.writeln(a);
+
+ let b = 1 + 1.2 + "a" +1;
+ document.writeln(b);
+
+ let c = " " ==0;// true
+
+ let d  = !(1+2==3)||!(2>4);//true
+
+ let e = 0.1 +0.2 ;
+ e = 0.3;
+ document.writeln(e);
+
+ let f = 9%2; //1
+ document.writeln(f);
+
+ let g = -1 / 0 ;
+    document.writeln(g);    g
+
+
+
+
 
 
 
