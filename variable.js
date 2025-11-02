70c9db1dd250fa7ee6a09f040cd552e869ad57ea
Nuriye10 <nadirnurani@gmail.com>
2025-11-02 22:08:09 +0100

    practiced string escape sequance

--- variable.js
+++ variable.js
@@ -10,6 +10,30 @@ let s = "12345 12345";
 let l = s.length;//
 document.writeln(l);
 
+let text = "My favourite book is 'Harry potter'";
+document.writeln(text);
+
+//String escape sequences
+// \' - single quote
+// \" - double quote
+// \b - backspace
+// \f -form feed 
+// \n - new line 
+// \r - carriage return 
+// \t- horizontal tabulator
+// \v - vertical tavulator
+
+let color = "My favourite color is \"burgundy\"";
+document.writeln(color);
+
+let myCoat = "My favourite coat\
+ is \"beige \"";
+document.writeln(myCoat);
+
+let text1 = "Nuriye Kader";
+let greetings ="welcom to noroff ";
+let message = greetings + text1;
+document.writeln(message);
 
 
 
