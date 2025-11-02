f99c7c7a5b3a7bd115d4fdd278492b779ca4c00a
Nuriye10 <nadirnurani@gmail.com>
2025-11-02 20:57:06 +0100

    started STRING

--- variable.js
+++ variable.js
@@ -1,28 +1,14 @@
- let mySentence = "I'm sentencs number 3 ,and I was printed from JS file " 
- document.writeln(mySentence);
+//String 
 
- //task 2
+let name = "Nadir";
+let name1 ='Nadir';
+let name2 = '';
+let name3 =' "Nadir"';
+// all above works, but we prefer to stick to one style.
 
- let a = "a" + 1+2+1.2;
- document.writeln(a);
-
- let b = 1 + 1.2 + "a" +1;
- document.writeln(b);
-
- let c = " " ==0;// true
-
- let d  = !(1+2==3)||!(2>4);//true
-
- let e = 0.1 +0.2 ;
- e = 0.3;
- document.writeln(e);
-
- let f = 9%2; //1
- document.writeln(f);
-
- let g = -1 / 0 ;
-    document.writeln(g);    g
-
+let s = "12345 12345";
+let l = s.length;//
+document.writeln(l);
 
 
 
