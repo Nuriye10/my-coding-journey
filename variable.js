b001409acf91ee05c6f45575ad07e6dc54f1aaf8
Nuriye10 <nadirnurani@gmail.com>
2025-11-03 21:42:28 +0100

    string methods

--- variable.js
+++ variable.js
@@ -1,18 +1,20 @@
-// Operations on strings
-
 let a = "abc";
-let b ="de";
-let c= "xyz";
-let d = a +b ;
- document.writeln(d);//abcde
-  let e = a.concat(c);
-  document.writeln(e);//abcxyz
+let b = "de";
+let c = "abcd";
 
-  let x = "abc";
-  let y = "de";
-  x += y;
-  document.writeln(x);//abcde
-;
+document.writeln(a >b);//false/document.writeln(b > c);// true
+document.writeln(a > c); //false
+document.writeln(a < b); // true
+document.writeln(b < c);//false
+document.writeln(a < c); //true
+
+let text = "abcdefghijk";
+let length = text.length;
+document.writeln(length);
+
+let text1 =" Hello people";
+let char = text1.charAt(7)
+document.writeln(char);
 
 
 
