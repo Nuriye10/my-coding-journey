1349353efcbbb8f1642c380d1372bb5ca8dc48cb
Nuriye10 <nadirnurani@gmail.com>
2025-11-03 20:57:21 +0100

    string operators

--- variable.js
+++ variable.js
@@ -1,40 +1,18 @@
-//String 
+// Operations on strings
 
-let name = "Nadir";
-let name1 ='Nadir';
-let name2 = '';
-let name3 =' "Nadir"';
-// all above works, but we prefer to stick to one style.
+let a = "abc";
+let b ="de";
+let c= "xyz";
+let d = a +b ;
+ document.writeln(d);//abcde
+  let e = a.concat(c);
+  document.writeln(e);//abcxyz
 
-let s = "12345 12345";
-let l = s.length;//
-document.writeln(l);
-
-let text = "My favourite book is 'Harry potter'";
-document.writeln(text);
-
-//String escape sequences
-// \' - single quote
-// \" - double quote
-// \b - backspace
-// \f -form feed 
-// \n - new line 
-// \r - carriage return 
-// \t- horizontal tabulator
-// \v - vertical tavulator
-
-let color = "My favourite color is \"burgundy\"";
-document.writeln(color);
-
-let myCoat = "My favourite coat\
- is \"beige \"";
-document.writeln(myCoat);
-
-let text1 = "Nuriye Kader";
-let greetings ="welcom to noroff ";
-let message = greetings + text1;
-document.writeln(message);
-
+  let x = "abc";
+  let y = "de";
+  x += y;
+  document.writeln(x);//abcde
+;
 
 
 
