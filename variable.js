5582e0534766165a96b4293a59e8e886071073e1
Nuriye10 <nadirnurani@gmail.com>
2025-11-30 19:50:30 +0100

    practiced some string methods

--- variable.js
+++ variable.js
@@ -1,23 +1,23 @@
-const str1 = 'Hello ';
-const str2 = 'My  ';
-const str3 = 'Pretty ';
-const str4 = 'World ';
-const str5 = str1+ str2+ str3+ str4;
+//Extraction of string elements
 
-document.writeln(str5);
-
- // their lexicographic order 
-const str6 ='Hello';
-const str7 = 'My';
-const str8 = 'Pretty';
-const str9 = 'World';
-// 
+let text = "My favorite color is \"Harry potter\"";
+let part = text.slice(8, 14);
+//document.writeln(part);//
+    console.log (part);
 
 
 
-     
+//negative slice
+let text1 = " My favourite book is \"Harry potter\"";
+let part1 = text1. slice(-27,-20);
+//document.writeln(part1);
+console.log (part1);
 
-
+let text2= " My hair color is black " 
+let part2 =text2.slice(4);
+document.writeln(part2);
+let part3 = text2.slice(-5);
+document.writeln(part3);
 
 
 
