dfb7d12b0816c9ab43503a42a2f773111c91b763
Nuriye10 <nadirnurani@gmail.com>
2025-12-13 17:40:43 +0100

    string practice

--- variable.js
+++ variable.js
@@ -1,24 +1,2 @@
-//Extraction of string elements
-
-let text = "My favorite color is \"Harry potter\"";
-let part = text.slice(8, 14);
-//document.writeln(part);//
-    console.log (part);
-
-
-
-//negative slice
-let text1 = " My favourite book is \"Harry potter\"";
-let part1 = text1. slice(-27,-20);
-//document.writeln(part1);
-console.log (part1);
-
-let text2= " My hair color is black " 
-let part2 =text2.slice(4);
-document.writeln(part2);
-let part3 = text2.slice(-5);
-document.writeln(part3);
-
-
-
+document.writeln("I'm sentence number 3 and I  was printed from the javascript file" );
 
