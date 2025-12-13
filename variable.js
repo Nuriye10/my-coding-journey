61c10253337f7a7cb75743a380b878671c1f5863
Nuriye10 <nadirnurani@gmail.com>
2025-12-13 19:29:27 +0100

    some example

--- variable.js
+++ variable.js
@@ -1,2 +1,22 @@
-document.writeln("I'm sentence number 3 and I  was printed from the javascript file" );
+//string- Escape sequences
 
+let text = "My favourite cake is \"Tiramisu\"";
+
+//   \' - Single quote
+//  \" - Double quote
+
+//  \\-backslash
+//  \b -backspace
+// \f-form feed 
+// \n - new line;
+// \r-carriage return 
+// \t -horizontal tabulator
+// \v -vertical tabulator 
+
+let message = "my favourite book\n \
+ is \"The sea\""; 
+  
+console.log(message);
+document.writeln(message);
+console .log(text);
+document.writeln(text);
