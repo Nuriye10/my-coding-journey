97d25768df270d71f8487f246d00ed5c26021e03
Nuriye10 <nadirnurani@gmail.com>
2025-12-14 19:35:43 +0100

    practice extraction of string elements

--- variable.js
+++ variable.js
@@ -1,18 +1,29 @@
-let a ="abc";
-let b= " def";
-let c ="ghtl";
+//Extraction of string elements
 
-let str1 = "hello";
-let str2 = "my ";
-let str3 =" pretty";
-let str5 = "world";
+//3 useful extracting methods:slice, substring, and substr
 
-let str6 = str1 +" " +str2 +" "+ str3+" "+str5;
-console.log (str6); //hello my  prettyworld
-document.writeln (str6); //hello my  prettyworld
+let text = "My favourite book is \"The sea\"";
 
+let part = text.slice(11, 19);
+console.log(part);
+document.writeln(part);
 
-document.writeln (a>b); //false
-document.writeln (a<b);//true
-document.writeln(c<a);//false
-document.writeln(c>b);//true
+let text2 = "My favourite book is \"the sea\"";
+let part2= text2.slice(-19, -11);
+console.log(part2);
+document.writeln(part2);
+
+let text3 = "My favourie book is \"the sea\"";
+let part3 =text3.slice(4);
+console.log(part3);
+document.writeln(part3);
+
+let text4 = "My favourite book is \"the sea\"";
+let part4 = text4.slice(-4);
+console.log(part4);
+document.writeln(part4);
+
+let text5 = "My favourite book is \"the sea\"";
+let part5 = text5.substr(4,5);
+console.log(part5);
+document.writeln(part5);
