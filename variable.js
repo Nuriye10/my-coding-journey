59eca384387a72367506aea050d6edf440f859d2
Nuriye10 <nadirnurani@gmail.com>
2025-12-16 22:21:00 +0100

    Boolean varbuls

--- variable.js
+++ variable.js
@@ -1,14 +1,7 @@
-//String search method 
-let text = "My favourite book is \" The sea\". I love to read this book again and again";
-let result= text.startsWith("I");
-let result2 = text.endsWith("again");
-let rsult3 = text.includes("is");
-let result4 =text.includes(" you");
-console.log(result);
-console.log(result2);
-console.log(rsult3);
-console.log(result4);
-document.writeln(result);
-document.writeln(result2);
-document.writeln(rsult3);
-document.writeln(result4);
+//Booleans
+
+let x = 5;
+x != 6;
+x== 6;
+console.log(x==6);
+console.log(x!=6);
