0de29c182dda21fc764e13dd016b5e0f202c3efe
Nuriye10 <nadirnurani@gmail.com>
2025-12-16 21:03:31 +0100

    String search method

--- variable.js
+++ variable.js
@@ -1,10 +1,12 @@
-//String-conversion toUpper() and toLowercCase()
+//String search method 
 
-let text  = "My favourite movie is \"Avatar\""; 
-let result = text.toLowerCase();
-let result1 = text.toUpperCase();
+let text = "My favourite movie is \"Avatar\". I like this movie very much ";
+let result =text.indexOf("movie");
+
+let text1 = "I love programming . especially \"JavaScript\"";
+let result1 =text1.indexOf("JavaScript");
 console.log(result1);
-console.log(result);
+document.writeln(result1);
 
 document.writeln(result);
-document.writeln(result1);
+console.log(result);
