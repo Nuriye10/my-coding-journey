a0ba5146bdb769b4b19f80020b4572b4971f6c73
Nuriye10 <nadirnurani@gmail.com>
2025-12-16 21:35:34 +0100

    practiced some tasks

--- variable.js
+++ variable.js
@@ -1,12 +1,14 @@
 //String search method 
-
-let text = "My favourite movie is \"Avatar\". I like this movie very much ";
-let result =text.indexOf("movie");
-
-let text1 = "I love programming . especially \"JavaScript\"";
-let result1 =text1.indexOf("JavaScript");
-console.log(result1);
-document.writeln(result1);
-
-document.writeln(result);
+let text = "My favourite book is \" The sea\". I love to read this book again and again";
+let result= text.startsWith("I");
+let result2 = text.endsWith("again");
+let rsult3 = text.includes("is");
+let result4 =text.includes(" you");
 console.log(result);
+console.log(result2);
+console.log(rsult3);
+console.log(result4);
+document.writeln(result);
+document.writeln(result2);
+document.writeln(rsult3);
+document.writeln(result4);
