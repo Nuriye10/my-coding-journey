c38f9903e9bf5051f04df66b26f89fccb674a85f
Nuriye10 <nadirnurani@gmail.com>
2025-12-20 21:23:32 +0100

    comments 2

--- tools.js
+++ tools.js
@@ -1,16 +1,12 @@
- 
-//Prompts
+//  line  comments
+//  let  x = 10 
+//  let y = 20;
+//  console.log (x);
 
-let x = prompt("Please pass the number between 1 to 4");
-if(x >3){
-    alert("Number is greater than 3"); 
-}else{
-    alert("Number is 3 or less");
-}
+// block comments
 
-let person = prompt("Please enter your name", "Rose Mary");
-if (person != null) {
-    alert ("Name is  provided");
-}else{
-    alert("Hello"+person+"!");
-}
+/*
+    let x = 10;
+    let y = 20;
+    console.log(x);
+*/
