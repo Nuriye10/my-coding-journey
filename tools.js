bed6b7f26f8ee3a77436c0d265428896dd9133bd
Nuriye10 <nadirnurani@gmail.com>
2025-12-20 23:07:11 +0100

    did the task

--- tools.js
+++ tools.js
@@ -4,23 +4,9 @@
 
 // console.log(  fruits[2]) ;
 
-const day = [];
-day [0]="sunday";
-day [1]="monday";
-day[2]="tuesday";
-day[3]="wednesday";
-day[4]="thursday";
-day[5]="friday";
-day[6]="saturday";
-console.log(day[3],day[5]);
-console.log (day.length);
-console.log(day.length -1);
 
-const Myhobbies =["reading", "coding","traveling","designing"];
-console.log(Myhobbies[1]);
-console.log(Myhobbies[3]);
-
-const Myinformations=["name:Nuriye", "age:38", "country:Turkey", "city:Istanbul"];
-console.log(Myinformations);
-console.log(Myinformations[0]);
-console.log(Myinformations[2]);
+const name = prompt('Enter your name:');
+const surname = prompt('Enter your surname:');
+const age = parseInt(prompt('Enter your age:'));
+const userData = [name, surname, age];
+console.log(userData);
