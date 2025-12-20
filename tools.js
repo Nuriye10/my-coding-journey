7cc640ff4be55ca2d44f44ce8807900f19311c64
Nuriye10 <nadirnurani@gmail.com>
2025-12-20 22:05:18 +0100

    arrays

--- tools.js
+++ tools.js
@@ -1,12 +1,17 @@
-//  line  comments
-//  let  x = 10 
-//  let y = 20;
-//  console.log (x);
+//Introduction to array methods in JavaScript
 
-// block comments
+// const fruits =["apple", "banana", "Mango","Orange"];
 
-/*
-    let x = 10;
-    let y = 20;
-    console.log(x);
-*/
+// console.log(  fruits[2]) ;
+
+const day = [];
+day [0]="sunday";
+day [1]="monday";
+day[2]="tuesday";
+day[3]="wednesday";
+day[4]="thursday";
+day[5]="friday";
+day[6]="saturday";
+console.log(day[3],day[5]);
+console.log (day.length);
+console.log(day.length -1);
