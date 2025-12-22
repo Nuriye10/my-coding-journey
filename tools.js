ad5c7a39e0f2d3da08548a2f1f922f3c1bafe672
Nuriye10 <nadirnurani@gmail.com>
2025-12-22 13:55:04 +0100

    some more lines

--- tools.js
+++ tools.js
@@ -1,6 +1,23 @@
 // Other array methods 
-//toString(), join()
+//split(), concat(),
 
-const teas=["green", "black", "dark", "fruit", "herbal"];
-console.log(teas.toString());
-console.log(teas.join("-"));
+const message ="Please, help me to cancel my order!";
+const words=message.split(" ");
+console.log(words);
+
+const information =" Albert Einstein, physicist, born 1879, died 1955.";
+const data=information.split(" ,");
+
+console.log(data);
+
+const cities = ["Oslo", "Moss", "Bergen",];
+const cities2=["Trondheim", "Stavanger"];
+const cities3=["Hamar", "Drammen"];
+const finalCities= cities.concat(cities2,cities3    );
+console.log(finalCities);
+const movie =["kongfu panda"];
+const movies =["Avatar", "gameof thrones"];
+const moreMovies = ["Inception", "The Matrix"];
+//const allMovies = movie.concat(movies, moreMovies);
+const allMovies=[...movie,...movies,...moreMovies];
+console.log(allMovies);
