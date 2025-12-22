9cc98b4f87265003cbeb7fa63f7ebc65b20df962
Nuriye10 <nadirnurani@gmail.com>
2025-12-22 22:47:54 +0100

    added new line

--- tools.js
+++ tools.js
@@ -1,23 +1,21 @@
 // Other array methods 
-//split(), concat(),
+//split(), concat(), splice(), slice(),
 
-const message ="Please, help me to cancel my order!";
-const words=message.split(" ");
-console.log(words);
+const numbers =[1,2,3,4,5,6];
+const newNumbers = numbers.slice(1,3);
+console.log(numbers);
+console.log(newNumbers);
 
-const information =" Albert Einstein, physicist, born 1879, died 1955.";
-const data=information.split(" ,");
+const  number1= [1,2,3,4,5,6,7,8];
+const number2= number1.slice(1,5);
+console.log(number1);
+console.log(number2);
 
-console.log(data);
+const number3= [1,2,3,4,5,6,7,8];
+const newnumber =number3.slice(-4,-1); 
+console.log(number3);
+console.log(newnumber);
 
-const cities = ["Oslo", "Moss", "Bergen",];
-const cities2=["Trondheim", "Stavanger"];
-const cities3=["Hamar", "Drammen"];
-const finalCities= cities.concat(cities2,cities3    );
-console.log(finalCities);
-const movie =["kongfu panda"];
-const movies =["Avatar", "gameof thrones"];
-const moreMovies = ["Inception", "The Matrix"];
-//const allMovies = movie.concat(movies, moreMovies);
-const allMovies=[...movie,...movies,...moreMovies];
-console.log(allMovies);
+const number4 = [1,2,3,4,5,6,7,8];
+number4.splice(2, 4, 9, );
+console.log(number4);
