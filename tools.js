3a8683d0c58d52bd5a7d23f8ae74bd736f5a369c
Nuriye10 <nadirnurani@gmail.com>
2025-12-23 23:27:58 +0100

    new line

--- tools.js
+++ tools.js
@@ -4,3 +4,9 @@
 const animals =["cat","dog","sheep","cow","elephant"];
 const newAnimals= animals.slice(1,4);
 console.log(newAnimals); //["dog","sheep","cow"]
+
+
+const greetings=[prompt("enter greeting 1") ];
+const greetings2=[prompt("enter greeting 2")];
+const allGreetings=greetings.concat(greetings2);
+console.log(allGreetings);
