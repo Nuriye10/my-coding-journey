cda498f101c9bc6b12921555e42edf60adfc8b7e
Nuriye10 <nadirnurani@gmail.com>
2025-12-23 23:22:37 +0100

    added new lines

--- tools.js
+++ tools.js
@@ -1,21 +1,6 @@
 // Other array methods 
-//split(), concat(), splice(), slice(),
+// function
 
-const numbers =[1,2,3,4,5,6];
-const newNumbers = numbers.slice(1,3);
-console.log(numbers);
-console.log(newNumbers);
-
-const  number1= [1,2,3,4,5,6,7,8];
-const number2= number1.slice(1,5);
-console.log(number1);
-console.log(number2);
-
-const number3= [1,2,3,4,5,6,7,8];
-const newnumber =number3.slice(-4,-1); 
-console.log(number3);
-console.log(newnumber);
-
-const number4 = [1,2,3,4,5,6,7,8];
-number4.splice(2, 4, 9, );
-console.log(number4);
+const animals =["cat","dog","sheep","cow","elephant"];
+const newAnimals= animals.slice(1,4);
+console.log(newAnimals); //["dog","sheep","cow"]
