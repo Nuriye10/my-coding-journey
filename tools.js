be7bb39e7acf94cdb27e1762ef5db6f4a4c1b58b
Nuriye10 <nadirnurani@gmail.com>
2025-12-24 16:26:51 +0100

    function

--- tools.js
+++ tools.js
@@ -1,12 +1,7 @@
 // Other array methods 
-// function
+// function: for each, map, filter,r
 
-const animals =["cat","dog","sheep","cow","elephant"];
-const newAnimals= animals.slice(1,4);
-console.log(newAnimals); //["dog","sheep","cow"]
-
-
-const greetings=[prompt("enter greeting 1") ];
-const greetings2=[prompt("enter greeting 2")];
-const allGreetings=greetings.concat(greetings2);
-console.log(allGreetings);
+function number(x){
+    return x * 2;
+}
+console.log(number(2));
