413cf23f2f918fe4ceff8aab15f579bcd00ed117
Nuriye10 <nadirnurani@gmail.com>
2025-12-24 18:44:08 +0100

    new lines

--- tools.js
+++ tools.js
@@ -1,31 +1,12 @@
+//multi demensional arrays
+
+// const numbers2d= [[1,2,3],[2,5,6],[7,8,9],[10,11,12]];
+// const numbers3d=[[[1,2],[3,4]],[[5,6],[7,8]],[[9,10],[11,12]]];
+// console.log(numbers2d[2],);
+// console.log(numbers3d[1][0][1]);
 
 
-   
-
-// const numbers =[1,2,3,4,5,6];
-// const even =numbers.filter(isEven);
-// console.log(numbers);
-// console.log(even);
-// function isEven(value){
-//     return value % 2 == 0;
-// }
-
-// const numbers= [1,2,3,4,5,6];
-// const sum = numbers .reduce ((total, value)=> total + value);
-// console.log(numbers);
-// console.log(sum);
-
-const colors = [];
-const color1 = prompt("enter your first favourite color");
-const color2 =prompt("enter your second favourite color");
-const color3= prompt("enter your third favourite color");
-
-colors.push(color1, color2, color3);
-
-const upperCaseColors = colors.map(function(color){
-    return color.toUpperCase();
-});
-console.log("Colors in capital letters:");
-console.log(upperCaseColors[0]);
-console.log(upperCaseColors[1]);
-console.log(upperCaseColors[2]);
+const numbers2d = [[0,1,2], [1,2,3], [1,2,4]];
+const numbers3d = [[[0],[1],[2,3]], [[0, 1],[2,3]], [[1],[1],[2,3]], [[1, 5],[2,3]]];
+console.log(numbers2d[0][1]); //1
+console.log(numbers3d[0][0][0]); //0
