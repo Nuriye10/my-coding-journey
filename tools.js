94f379a96fac8a3483357eb7df0ddb156710fbf4
Nuriye10 <nadirnurani@gmail.com>
2025-12-25 18:37:09 +0100

    did some task

--- tools.js
+++ tools.js
@@ -1,4 +1,4 @@
-//loops
+loops
 for (let i =0;  i <=100; i++){
     console.log(i);
 }
@@ -11,4 +11,14 @@ for(let i =0 ; i <= NuriyesFavThings.length-1; i++){
 const weekDays= ["monday","tuesday", "wednesday", "thursday", "friday"];
 for (let i =0; i <= weekDays.length -3; i++){
     console.log(weekDays[i]);
+}
+
+const number =[1,2,3,4,5,6,7,8,9,10];
+for(let i =0; i <=10; i++) {
+    console.log(number[i]);
+}
+
+const array =[1, 2, 3, "hello", "Hi", true, false , 2.34, NaN, null];
+for (let i =0; i < 10 ; i++){
+    console.log(array[i]);
 }
