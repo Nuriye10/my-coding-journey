fe6f1faf14af90414d7948839b38187a50dc7cce
Nuriye10 <nadirnurani@gmail.com>
2025-12-25 18:57:21 +0100

    loop scope

--- tools.js
+++ tools.js
@@ -1,24 +1,12 @@
-loops
-for (let i =0;  i <=100; i++){
+// The loop scope
+
+var i =5;
+for (var i =0; i< 10; i++){
+
+    console.log(i);//
+}
+
+let i =5;
+for (let i =0; i < 10; i++){
     console.log(i);
-}
-const NuriyesFavThings = ["cake", "Coffee", "book", "shopping", "coding"];
-
-for(let i =0 ; i <= NuriyesFavThings.length-1; i++){
-    console.log(NuriyesFavThings[i]);
-}
-
-const weekDays= ["monday","tuesday", "wednesday", "thursday", "friday"];
-for (let i =0; i <= weekDays.length -3; i++){
-    console.log(weekDays[i]);
-}
-
-const number =[1,2,3,4,5,6,7,8,9,10];
-for(let i =0; i <=10; i++) {
-    console.log(number[i]);
-}
-
-const array =[1, 2, 3, "hello", "Hi", true, false , 2.34, NaN, null];
-for (let i =0; i < 10 ; i++){
-    console.log(array[i]);
-}
+}//
