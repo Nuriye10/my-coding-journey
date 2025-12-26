2af010b0b37c2a8c61ea22c9683b0f9c97a6b0c7
Nuriye10 <nadirnurani@gmail.com>
2025-12-26 23:47:10 +0100

    some new lines

--- tools.js
+++ tools.js
@@ -1,18 +1,38 @@
 // The while loop//do while loop
 
-  const shoppingList= ["milk", "bread", "eggs", "Butter", "cheese"];
-  let i =0;
- while(i <shoppingList.length){
-    console.log(shoppingList[i]);
-    i++;
- }  
+//   const shoppingList= ["milk", "bread", "eggs", "Butter", "cheese"];
+//   let i =0;
+//  while(i <shoppingList.length){
+//     console.log(shoppingList[i]);
+//     i++;
+//  }  
 
-// do while loop
-do{
-    console.log(shoppingList[i]);
-    i++;
+// // do while loop
+// do{
+//     console.log(shoppingList[i]);
+//     i++;
 
-}while(i <shoppingList.length);
+// }while(i <shoppingList.length);
 
 
+//   const array = [];
+//   for(let i = 1; i<20; i++){
+//     array.push(i);
+//   }  
+//   for (let i =0; i<array.length; i++){
+//     if (array[i]% 7==0){
+//         console.log("found first multiplicity of 7!: " + array[i]);
+//         break;
+//     }
+//     else{
+//         console.log(array[i]);
 
+//     }
+//     }
+
+const todoList =["cleaning", "shopping", "studying", "exercise"];
+let index =0;
+while(index < todoList.length){
+    console.log(todoList[index]);
+    index++;
+}
