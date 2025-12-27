267b4515f0ccc0da94aeb516caa9becd8a7065d8
Nuriye10 <nadirnurani@gmail.com>
2025-12-27 21:15:13 +0100

    new lines

--- tools.js
+++ tools.js
@@ -1,11 +1,30 @@
-//disrupting the loop-break and continue
-    //break statement
-while(true){
-    let result =prompt("provide an even number.");
-    if (result % 2 === 0){
-       
-    alert(result);
-    break;
+// //loops/ introduction to functions
 
-       } 
- }
+// function multiply(a, b){
+//     return a * b;
+// }
+
+// function multiply(a,b){
+//     return a *b;
+// }
+// multiply(4,5);
+
+const items=["apple","banana","orange"];
+
+function add(a ,b){
+    return a+b;
+}
+add(5,10);
+console.log(add(5,10));
+
+function multiply(a,b){
+    return a*b;
+}
+multiply(2,3);
+console.log(multiply(2,3)
+);
+
+function add(c,d){
+    console.log(c+d);
+}
+add(2,8);
