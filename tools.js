b6ab94234c432e13a56952e511aa4c85852fd726
Nuriye10 <nadirnurani@gmail.com>
2025-12-27 20:30:54 +0100

    did the task

--- tools.js
+++ tools.js
@@ -1,26 +1,11 @@
 //disrupting the loop-break and continue
     //break statement
-for (let i =0; i<10; i++){
-    if (i==5){
-        break;
-    }
-    console.log(i);
-}
-
-//continue statement
-let shoppingList=['milk', 'bread', 'eggs', 'butter', 'cheese'];
-for(let i =0; i<shoppingList.length; i++){
-    if (shoppingList[i]==='eggs'){
-        continue;
-    }
-    console.log(shoppingList[i]);
-}
-
-let i = 0;
 while(true){
-    i++;
-    if(i >=10){
-        break;
-    }
-    console.log(i);
-}
+    let result =prompt("provide an even number.");
+    if (result % 2 === 0){
+       
+    alert(result);
+    break;
+
+       } 
+ }
