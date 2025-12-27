4119d5cac98b92c0ed1655045d94c9c690aafc5c
Nuriye10 <nadirnurani@gmail.com>
2025-12-27 21:45:49 +0100

    some exercies

--- tools.js
+++ tools.js
@@ -1,30 +1,12 @@
-// //loops/ introduction to functions
 
-// function multiply(a, b){
-//     return a * b;
-// }
 
-// function multiply(a,b){
-//     return a *b;
-// }
-// multiply(4,5);
+    function add (x,y,z){
+        console.log(x+y+z);
+    }
+    add(2,3,4);
 
-const items=["apple","banana","orange"];
-
-function add(a ,b){
-    return a+b;
-}
-add(5,10);
-console.log(add(5,10));
-
-function multiply(a,b){
-    return a*b;
-}
-multiply(2,3);
-console.log(multiply(2,3)
-);
-
-function add(c,d){
-    console.log(c+d);
-}
-add(2,8);
+    function multiply(a,b,c){
+        return a*b*c;
+    }
+    const result =multiply(2,3,4);
+    console.log(result);
