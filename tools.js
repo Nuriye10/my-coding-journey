ed83a1792f243ebac5d7da4abdba7c5e6f5d92f7
Nuriye10 <nadirnurani@gmail.com>
2025-12-29 23:49:34 +0100

    added newline

--- tools.js
+++ tools.js
@@ -3,13 +3,16 @@
 const food = ["milk","bread", "eggs", "butter"];
 
 const fruit = ["mango", "orange", "grape", "pear"];
-
+const shoppingList=["soap", "shampoo","toothpaste", "toothbrush"];
 const printArray =(array)=>{
     for (let i = 0; i<=array.length-1; i++){
         console.log(array[i]);
         document.writeln(array[i]);
     }
 }
+
+
+
 
 
 
