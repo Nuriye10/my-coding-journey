b43e82259f0d58fc6f795218a1a85af9d5b362be
Nuriye10 <nadirnurani@gmail.com>
2025-12-31 22:54:21 +0100

    more lines

--- tools.js
+++ tools.js
@@ -1,12 +1,20 @@
 //classes
 
-class ClassName {
-    constructor(){}
-    method_1(){}
-    method_2(){}
-    method_3(){}   
-}
+// class ClassName {
+//     constructor(){}
+//     method_1(){}
+//     method_2(){}
+//     method_3(){}   
+// }
 
 
-
-
+class Book {
+    constructor(title, author, publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+    age() {
+        let currentDate = new Date();
+        return currentDate.getFullYear() - this.publicationYear;
+    }
