c585cab70e09627c7f9e95a03151572141b2bc5e
Nuriye10 <nadirnurani@gmail.com>
2026-01-02 21:08:27 +0100

    classes

--- tools.js
+++ tools.js
@@ -7,14 +7,22 @@
 //     method_3(){}   
 // }
 
+//How to create a simple class : 
 
-class Book {
-    constructor(title, author, publicationYear) {
-        this.title = title;
-        this.author = author;
-        this.publicationYear = publicationYear;
-    }
-    age() {
-        let currentDate = new Date();
-        return currentDate.getFullYear() - this.publicationYear;
-    }
+class cake {
+  constructor (flavour, color, shape){
+    this.flavour = flavour;
+    this.color = color;
+    this.shape = shape;
+  }
+
+  form() {
+    let currentForm = new Form();
+    return currentForm.shape();
+  }
+}
+let newCake = new cake("chocolate", "brown", "circule");
+console.log(newCake.flavour);//chocolate
+console.log(newCake.color);//brown
+console.log(newCake.shape);//
+  
