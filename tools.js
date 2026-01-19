baef994f0b101dad99d3dbde344e03d43d9f10f3
Nuriye10 <nadirnurani@gmail.com>
2026-01-19 20:17:19 +0100

    added new lines

--- tools.js
+++ tools.js
@@ -13,3 +13,23 @@ let flower = new FlowerName("Rose", "White", "Spring");
 console.log(flower.name);      // Output: Rose
 console.log(flower.color);     // Output: White
 console.log(flower.getSeason()); // Output: Spring
+
+class Student{
+  constructor(name, surname, age){
+    this.name=name;
+    this.surname=surname;
+    this.age=age;
+  }
+  getGreetings(){
+    return `Hello, my name is ${this.name} ${this.surname} and I am ${this.age} years old,`;
+  }
+
+getBirthYear(){
+  return new Date().getFullYear() - this.age;
+}
+}
+
+let student1 = new Student("Nuriye","Kader", 38);
+console.log(student1.getGreetings());
+console.log("Birth Year: " + student1.getBirthYear());
+
