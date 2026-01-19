b0352e5d3f30b50333c73ce65ede6cc2d95a39e2
Nuriye10 <nadirnurani@gmail.com>
2026-01-19 19:38:21 +0100

    new example

--- tools.js
+++ tools.js
@@ -1,85 +1,15 @@
-//classes
-
-// class ClassName {
-//     constructor(){}
-//     method_1(){}
-//     method_2(){}
-//     method_3(){}   
-// }
-
-//How to create a simple class : 
-
-// class cake {
-//   constructor (flavour, color, shape){
-//     this.flavour = flavour;
-//     this.color = color;
-//     this.shape = shape;
-//   }
-
-//   form() {
-//     let currentForm = new Form();
-//     return currentForm.shape();
-//   }
-// }
-// let newCake = new cake("chocolate", "brown", "circule");
-// console.log(newCake.flavour);//chocolate
-// console.log(newCake.color);//brown
-// console.log(newCake.shape);//
-  
-
-class house {
-    constructor(rooms, year, type) {
-        this.room = rooms;
-        this.year = year;
-        this.type = type;
-    }
-
-    floor() {
-        // Placeholder logic, as 'type' is a string, not a class
-        return this.type;
-    }
+class FlowerName {
+	constructor(name, color, season) {
+		this.name = name;
+		this.color = color;
+		this.season = season;
+	}
+	getSeason() {
+		return this.season;
+	}
 }
 
-let newHouse = new house(5, 2024, "villa");
-console.log(newHouse.room); //5
-console.log(newHouse.year); //2024
-console.log(newHouse.type); //villa
-//console.log(newHouse.floor()); //villa
-
-
-
-
-class Animal {
-  constructor(name) {
-    this.name = name;
-  }
-
-  speak() {
-    console.log(`${this.name} makes a sound.`);
-  }
-}
-
-const dog = new Animal('Dog');
-dog.speak(); // Output: Dog makes a sound.
-
-class Car {
-  constructor(brand, year) {
-    this.brand = brand;
-    this.year = year;
-  }
-
-  honk() {
-    console.log("Beep beep!");
-  }
-}
-
-class Rectangle {
-  constructor(width, height) {
-    this.width = width;
-    this.height = height;
-  }
-
-  area() {
-    return this.width * this.height;
-  }
-}
+let flower = new FlowerName("Rose", "White", "Spring");
+console.log(flower.name);      // Output: Rose
+console.log(flower.color);     // Output: White
+console.log(flower.getSeason()); // Output: Spring
