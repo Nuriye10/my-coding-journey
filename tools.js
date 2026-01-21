2b34c059181c95c8a457f0c0c5951e2f4f58426a
Nuriye10 <nadirnurani@gmail.com>
2026-01-21 19:36:31 +0100

    encapsulation

--- tools.js
+++ tools.js
@@ -1,64 +1,33 @@
-// class FlowerName {
-// 	constructor(name, color, season) {
-// 		this.name = name;
-// 		this.color = color;
-// 		this.season = season;
-// 	}
-// 	getSeason() {
-// 		return this.season;
-// 	}
-// }
+// Encapsulation
 
-// let flower = new FlowerName("Rose", "White", "Spring");
-// console.log(flower.name);      // Output: Rose
-// console.log(flower.color);     // Output: White
-// console.log(flower.getSeason()); // Output: Spring
-
-// class Student{
-//   constructor(name, surname, age){
-//     this.name=name;
-//     this.surname=surname;
-//     this.age=age;
-//   }
-//   getGreetings(){
-//     return `Hello, my name is ${this.name} ${this.surname} and I am ${this.age} years old,`;
-//   }
-
-// getBirthYear(){
-//   return new Date().getFullYear() - this.age;
-// }
-// }
-
-// let student1 = new Student("Nuriye","Kader", 38);
-// console.log(student1.getGreetings());
-// console.log("Birth Year: " + student1.getBirthYear());
-
-
-
-class Student {
-  constructor (name, surname, age){
+class Car {
+  constructor(name, year) {
     this.name = name;
-    this.surname=surname;
-    this.age = age;
+    this.year = year; // This will use the setter
   }
-  greetings(){
-    return `Hello my name is ${this.name} ${this.surname}.`;
+  set year(newYear) {
+    const date = new Date();
+    if (newYear > date.getFullYear()) {
+      this._year = date.getFullYear();
+    } else {
+      this._year = newYear;
+    }
   }
-    birthYear() {
-    let date =new Date();
-    return date.getFullYear() -this.age;
+  get year() {
+    return this._year;
   }
-    }  
-  const student1 = new Student("Nuriye", "Kader", 38);
-  const student2 = new Student ("John", "Doe", 29);
-  const student3 = new Student ("Jane", "Smith", 36);
-  console.log(student1.greetings());
-  console.log(student1.birthYear());
-  console.log(student2.greetings());
-  console.log(student2.birthYear());
-  console.log(student3.greetings());
-  console.log(student3.birthYear());
+  age() {
+    const date = new Date();
+    return date.getFullYear() - this._year;
+  }
+}
 
-  
+let myCar = new Car("Ford", 2025);
+console.log(myCar.year);      // Uses getter
+console.log(myCar._year);     // Direct access (not recommended)
+myCar._year = 2020;           // Directly setting _year (bypasses setter)
+console.log(myCar.year);      // Uses getter
+console.log(myCar.age());     // Uses age method
+
   
 
