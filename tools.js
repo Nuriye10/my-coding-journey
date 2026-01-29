dfa6c3dda24f03f6781224c27e9c3eab717c5991
Nuriye10 <nadirnurani@gmail.com>
2026-01-29 13:50:11 +0100

    new line added

--- tools.js
+++ tools.js
@@ -1,12 +1,33 @@
-//Encapsulation
+//encapsulation
 
-class car {
-   constructor(name,year) {
-    this.name=name;
-    this.year=year;
+class Car {
+  constructor(brand, year){
+   this.brand=brand;
+   this.year = year;
+  }
+  set year(newYear){
+   let date= new Date();
+   if(newYear>date.getFullYear()){
+      this._year =date.getFullYear();
    }
+   else{
+      this._year =newYear
+   }
+  }
 
-} 
-let mycar=new car("ford",2020);
-console.log(mycar.name);  
-console.log(mycar.year);
+ get year(){
+   return this._year;
+ }
+ age(){
+   let date=new Date();
+   return date.getFullYear() - this.year;
+ }
+}a
+
+
+let newCar = new Car("citron", 2022);
+console.log(newCar.year);//
+console.log(newCar._year)//
+newCar._year = 2250;//
+console.log(newCar.year);//
+console.log(newCar._year)//
