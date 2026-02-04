e57fbf3fe9937a8670e8ed6f3d22920f0b1c6c52
Nuriye10 <nadirnurani@gmail.com>
2026-02-04 21:39:17 +0100

    newline added

--- tools.js
+++ tools.js
@@ -1,33 +1,59 @@
-//encapsulation
+// //encapsulation
 
-class Car {
-  constructor(brand, year){
-   this.brand=brand;
-   this.year = year;
+// class Car {
+
+//    #_year
+//    constructor(name, year ){
+//     this.name =name;
+//     this.year=year;
+
+//    }
+//    set year (newYear){
+//       let date =new Date();
+//       if(newYear > date.getFullYear()){
+//          this.#_year=date.getFullYear();
+//       }else{
+//          this.#_year=newYear;
+
+//       }
+
+//    }
+//    get year (){
+//       return this.#_year;
+//    }
+//    age(){
+//       let date= new Date();
+//       return date.getFullYear() -this.#_year;
+//    }
+
+// }
+// let newCar =new Car("Nissan", 2220);//
+// console.log(newCar.year);
+// console.log(newCar.age());
+
+
+
+
+class Person {
+  constructor(name, age) {
+    let _age = age; // private variable
+
+    this.name = name;
+
+    this.getAge = function() {
+      return _age;
+    };
+
+    this.setAge = function(newAge) {
+      if (newAge > 0) {
+        _age = newAge;
+      }
+    };
   }
-  set year(newYear){
-   let date= new Date();
-   if(newYear>date.getFullYear()){
-      this._year =date.getFullYear();
-   }
-   else{
-      this._year =newYear
-   }
-  }
+}
 
- get year(){
-   return this._year;
- }
- age(){
-   let date=new Date();
-   return date.getFullYear() - this.year;
- }
-}a
-
-
-let newCar = new Car("citron", 2022);
-console.log(newCar.year);//
-console.log(newCar._year)//
-newCar._year = 2250;//
-console.log(newCar.year);//
-console.log(newCar._year)//
+const person = new Person("Alice", 25);
+console.log(person.name);      // Alice
+console.log(person.getAge());  // 25
+person.setAge(30);
+console.log(person.getAge());  // 30
